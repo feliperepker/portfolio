27d93a092be260326cd04b49f1703406e1f40434
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2024-03-29 18:35:10 -0300

    fix the mountain image

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,55 +1,35 @@
 let show = true;
 
-const menuSection = document.querySelector('.menu-section')
-const menuToggle = menuSection.querySelector('.hamburguer-menu')
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = menuSection.querySelector(".hamburguer-menu");
 
-menuToggle.addEventListener('click', () =>{
-    document.body.style.overflow = show ? 'hidden' : 'initial'; 
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
 
-    menuSection.classList.toggle('on', show)
-    show = !show;
-})
+  menuSection.classList.toggle("on", show);
+  show = !show;
+});
 
-
-const observer = new IntersectionObserver(entries => {
-    Array.from(entries).forEach(entry => {
-        if(entry.intersectionRatio >= 0.05){
-            entry.target.classList.add('init-hidden-off')
-        } 
-    })
-  }, {
-    threshold: [0, 0, 0.1]
-  })
-  Array.from(document.querySelectorAll('.init-hidden')).forEach(element =>{
-    observer.observe(element)
-  })
-
-var coffe = document.querySelector('.cupCoffe')
-var sorry = document.querySelector('.sorry')
-coffe.addEventListener('click', ()=>{
-  console.log('oi')
-  coffe.classList.add('animationcoffe')
-  sorry.classList.remove('hide')
-})
-
-let montanhaVerde = document.querySelector('.montanhaverde')
-let montanhaNeve = document.querySelector('.montanhaneve')
-
-window.addEventListener('scroll', function(){
-  var value = window.scrollY
-
-
-  var x = window.matchMedia("(max-width: 700px)")
-  var y = window.matchMedia("(max-width: 1500px)")
-  if(x.matches){
-    montanhaVerde.style.top = (value +760) * 0.3 + 'px'
-    montanhaNeve.style.top = (value + 170)* 0.9 + 'px'
-    
-  }else if(y.matches){
-    montanhaVerde.style.top = (value +500) * 0.3 + 'px'
-    montanhaNeve.style.top = (value + 5)* 0.9 + 'px'
-  }else{
-    montanhaVerde.style.top = (value +800) * 0.3 + 'px'
-    montanhaNeve.style.top = (value + 5)* 0.9 + 'px'
+const observer = new IntersectionObserver(
+  (entries) => {
+    Array.from(entries).forEach((entry) => {
+      if (entry.intersectionRatio >= 0.05) {
+        entry.target.classList.add("init-hidden-off");
+      }
+    });
+  },
+  {
+    threshold: [0, 0, 0.1],
   }
-})
+);
+Array.from(document.querySelectorAll(".init-hidden")).forEach((element) => {
+  observer.observe(element);
+});
+
+var coffe = document.querySelector(".cupCoffe");
+var sorry = document.querySelector(".sorry");
+coffe.addEventListener("click", () => {
+  console.log("oi");
+  coffe.classList.add("animationcoffe");
+  sorry.classList.remove("hide");
+});
